907dadf0e08184446e5d74f12049d7280229b2d1
Manish Kabra <manishkabra000@gmail.com>
2021-07-30 19:37:10 +0530

    register & login user

--- routes/auth.js
+++ routes/auth.js
@@ -21,4 +21,22 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// Login User
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User Not Found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Wrong Password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
